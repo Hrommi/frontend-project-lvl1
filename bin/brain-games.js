5302befbb3d78a159d970e20e3135569f801ba76
Stepan <goodhrommi@gmail.com>
2020-05-06 17:51:41 +0300

    change named export to default in cli.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { greetUserName } from '../src/cli.js';
+import greetUserName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 greetUserName();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const greetUserName = () => {
+const greetUserName = () => {
   const userName = readlineSync.question('May I have your name?');
   console.log(`Hello, ${userName}!`);
 };
+
+export default greetUserName;
