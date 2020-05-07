60e12ea1997d4f4da3c70c7c83b4a3731abf3ada
Stepan <goodhrommi@gmail.com>
2020-05-07 14:55:08 +0300

    refactor brain-even.js and even.js with new game runner

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,9 +2,10 @@
 
 import prompt from '../src/cli.js';
 import greeting from '../src/greeting.js';
+import startGame from '../src/index.js';
 import evenGame from '../src/games/even.js';
 
 console.log('Welcome to the Brain Games!');
 const username = prompt('May I have your name? ');
 greeting(username);
-evenGame(username);
+startGame(evenGame, username);
--- src/games/even.js
+++ src/games/even.js
@@ -1,29 +1,21 @@
 import getRandomNum from '../utils/getRandomNum.js';
 import isEven from '../utils/isEven.js';
-import prompt from '../cli.js';
 
-const even = (username) => {
-  const loseMessage = `Let's try again, ${username}`;
-  const winMessage = `Congratulations, ${username}!`;
-
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  for (let i = 0; i < 3; i += 1) {
+const even = () => {
+  const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
+  const start = () => {
     const num = getRandomNum(0, 99);
     const expectedAnswer = isEven(num) ? 'yes' : 'no';
-    console.log(`Question: ${num}`);
-    const answer = prompt('Your answer: ');
+    return {
+      question: num,
+      expectedAnswer,
+    };
+  };
 
-    if (expectedAnswer === answer) {
-      console.log('Correct!');
-    } else {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${expectedAnswer}".`);
-      console.log(loseMessage);
-      return;
-    }
-  }
-
-  console.log(winMessage);
+  return {
+    rules,
+    start,
+  };
 };
 
 export default even;
