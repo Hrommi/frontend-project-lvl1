1c9306cf51171d44937a33a0d79a842ba4549064
Stepan <goodhrommi@gmail.com>
2020-05-07 13:50:10 +0300

    create game runner

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import prompt from './cli.js';
+
+const startGame = (game, username) => {
+  const loseMessage = `Let's try again, ${username}`;
+  const winMessage = `Congratulations, ${username}!`;
+  const { rules, start } = game();
+
+  console.log(rules);
+
+  for (let i = 0; i < 3; i += 1) {
+    const { question, expectedAnswer } = start();
+    console.log(`Question: ${question}`);
+    const answer = prompt('Your answer: ');
+
+    if (expectedAnswer === answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${expectedAnswer}".`);
+      console.log(loseMessage);
+      return;
+    }
+  }
+
+  console.log(winMessage);
+};
+
+export default startGame;
