c2b4ebb06cdfec43759ce2fa9f12536a945fb49f
Stepan <goodhrommi@gmail.com>
2020-05-07 15:57:29 +0300

    create gcd game

---
+++ src/games/gcd.js
@@ -0,0 +1,22 @@
+import getRandomNum from '../utils/getRandomNum.js';
+import getGcd from '../utils/getGcd.js';
+
+const jcd = () => {
+  const rules = 'What is the result of the expression?';
+  const start = () => {
+    const num1 = getRandomNum(0, 10);
+    const num2 = getRandomNum(0, 10);
+    const expectedAnswer = String(getGcd(num1, num2));
+    return {
+      question: `${num1} ${num2}`,
+      expectedAnswer,
+    };
+  };
+
+  return {
+    rules,
+    start,
+  };
+};
+
+export default jcd;
