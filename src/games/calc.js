961894c36322868f7895668d6d19e981ae574a9e
Stepan <goodhrommi@gmail.com>
2020-05-31 14:02:29 +0300

    rename variable result to calculate in calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,15 +3,15 @@ import getRandomNum from '../utils/getRandomNum.js';
 const mapping = {
   0: {
     operator: '+',
-    result: (a, b) => a + b,
+    calculate: (a, b) => a + b,
   },
   1: {
     operator: '-',
-    result: (a, b) => a - b,
+    calculate: (a, b) => a - b,
   },
   2: {
     operator: '*',
-    result: (a, b) => a * b,
+    calculate: (a, b) => a * b,
   },
 };
 
@@ -25,7 +25,7 @@ const getCalcGame = () => {
     const operator = getOperator();
     const num1 = getRandomNum(0, 10);
     const num2 = getRandomNum(0, 10);
-    const expectedAnswer = String(operator.result(num1, num2));
+    const expectedAnswer = String(operator.calculate(num1, num2));
     return {
       question: `${num1} ${operator.operator} ${num2}`,
       expectedAnswer,
