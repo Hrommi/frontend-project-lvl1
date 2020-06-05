1038a8bf5ed90e8232008f35a7cf7930aa8ee916
Stepan <goodhrommi@gmail.com>
2020-06-05 19:12:39 +0300

    change mapping logic operators in calc game

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,34 +1,31 @@
 import getRandomNum from '../utils/getRandomNum.js';
 import startGame from '../index.js';
 
-const mapping = {
-  0: {
+const operators = [
+  {
     operator: '+',
     calculate: (a, b) => a + b,
   },
-  1: {
+  {
     operator: '-',
     calculate: (a, b) => a - b,
   },
-  2: {
+  {
     operator: '*',
     calculate: (a, b) => a * b,
   },
-};
-
-const getOperator = () => (
-  mapping[getRandomNum(0, Object.keys(mapping).length - 1)]
-);
+];
 
 const rules = 'What is the result of the expression?';
 
 const startCalcGame = () => {
-  const operator = getOperator();
+  const operatorIndex = getRandomNum(0, operators.length - 1);
+  const { operator, calculate } = operators[operatorIndex];
   const num1 = getRandomNum(0, 10);
   const num2 = getRandomNum(0, 10);
-  const expectedAnswer = String(operator.calculate(num1, num2));
+  const expectedAnswer = String(calculate(num1, num2));
   return {
-    question: `${num1} ${operator.operator} ${num2}`,
+    question: `${num1} ${operator} ${num2}`,
     expectedAnswer,
   };
 };
